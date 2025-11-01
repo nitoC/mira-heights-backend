a4ae7192ac81f408827b7fe25646033e6b2dc265
nitoc <daren2track@gmail.com>
2025-11-01 19:02:41 +0100

    fix: add cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://www.miraheightsng.com/'],
+    origin: [
+      'http://localhost:3000',
+      'https://www.miraheightsng.com',
+      'https://miraheightsng.com',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
