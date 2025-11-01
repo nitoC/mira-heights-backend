a1f4923d2044d89a3650923f8972acd95d1dfd48
nitoc <daren2track@gmail.com>
2025-11-01 18:34:05 +0100

    feat: add cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://mira-heights-backend-g71r.onrender.com/',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
